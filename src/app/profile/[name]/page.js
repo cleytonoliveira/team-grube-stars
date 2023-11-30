e06d60feaaa10955c8fc0f6b2c4c506c72759229
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2023-11-30 11:13:44 -0300

    chore: fix variable name

--- src/app/profile/[name]/page.js
+++ src/app/profile/[name]/page.js
@@ -1,13 +1,13 @@
 "use client";
-import { useRouter, useParams } from "next/navigation";
+import { useParams } from "next/navigation";
 
 export default function AthletePage() {
   const { name } = useParams();
-  const decode = decodeURIComponent(name);
+  const decodedName = decodeURIComponent(name);
   return (
     <div className="h-screen bg-[#1c1c1c]">
       <h1 className="text-5xl p-10 text-center font-bold font-sans text-[#FFC927]">
-        {decode}
+        {decodedName}
       </h1>
     </div>
   );
